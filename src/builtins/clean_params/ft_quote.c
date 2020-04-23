0073239435bd33b189cb8a33c8469eba903d6bf8
bsteve456 <stevealan1@protonmail.com>
2020-04-23 14:11:15 +0200

    refactoring ft_quote file

--- src/builtins/clean_params/ft_quote.c
+++ src/builtins/clean_params/ft_quote.c
@@ -6,10 +6,7 @@ int		count_env(char *params, int index , int c_env,t_env_lst *lst)
 	while(lst)
 	{
 		if(ft_memcmp(params, lst->name, c_env) == 0)
-		{
-			printf("ok\n");
 			return (ft_strlen(lst->value));
-		}
 		lst = lst->next;
 	}
 	return (2);
@@ -43,39 +40,40 @@ int		count_qchar(char *params, t_env_lst *lst, int quote_type, int index)
 	return (count);
 }
 
+void	dollar_quote(char *params, char **res, int *i, t_env_lst *lst)
+{
+	char *mem;
+	int j;
+	
+	j = 0;
+	mem = env_value2(params + 1, lst);
+	while(mem[j])
+		(*res)[(*i)++] = mem[j++];
+}
 
 char	*ft_quote(char *params, int index, t_env_lst *lst)
 {
 	int n;
 	char *res;
-	char *mem;
 	int j;
 	int i;
 
 	n = count_qchar(params, lst, params[index], index);
 	if(!(res = ft_calloc(n + 1, sizeof(char *))))
 		return (NULL);
-	n = params[index];
 	i = 0;
-	while(index >= 0)
+	j = 0;
+	while(j <= index)
 	{
-		if (*params == '$' && n == 34)
+		if (params[j] == '$' && params[index] == 34)
 		{
-			j = 0;
-			mem = env_value2(params + 1, lst);
-			while(mem[j])
-				res[i++] = mem[j++];
-			while(*(params + 1) != 34)
-			{
-				params++;
-				index--;
-			}
-			free(mem);
+			dollar_quote(params + j, &res, &i, lst);
+			while(params[j + 1] != 34)
+				j++;
 		}
-		else if(*params != 34)
-			res[i++] = *params;
-		params++;
-		index--;
+		else if(params[j] != 34)
+			res[i++] = params[j];
+		j++;
 	}
 	res[i] = '\0';
 	return (res);
