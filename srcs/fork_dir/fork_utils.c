1a6f8700001844483752c8f0580512990294a66c
bsteve456 <stevealan1@protonmail.com>
2020-05-07 20:07:29 +0200

    add normal_fork and fork_loop

--- srcs/fork_dir/fork_utils.c
+++ srcs/fork_dir/fork_utils.c
@@ -4,21 +4,21 @@ char *prs_mem(int fd)
 {
 	char *buf;
 	char *cumul;
-//	char *mem;
+	char *mem;
 
 	cumul = NULL;
 	if(!(buf = ft_calloc(100, sizeof(char))))
 		return (NULL);
-/*	while (read(fd, buf, 100)  != 0)
+	while (read(fd, buf, 100)  != 0)
 	{
-		printf("iii");
+//		printf("iii");
 		mem = cumul;
 		cumul = ft_strjoin(cumul, buf);
 		free(mem);
 	}
 	free(buf);
-	close(fd); */
-	read(fd, buf, 10);
-	printf("buf : [%s]\n", buf);
+	close(fd); 
+//	read(fd, buf, 10);
+//	printf("buf : [%s]\n", buf);
 	return (cumul);
 }
--- srcs/fork_dir/normal_fork.c
+++ srcs/fork_dir/normal_fork.c
@@ -3,32 +3,40 @@
 char *normal_fork(t_cmd *lst, t_data *data)
 {
 	int fd[2];
+	int save_fd;
 	char *mem;
 //	printf("hello\n");
-//	pid_t p;
+	pid_t p;
 //(void)data;
 //(void)lst;
 
 //if (!(fd = malloc(sizeof(int) * 2)))
 //		return (NULL);
 	mem = NULL;
-	pipe(fd);
-//	p = fork();
-	if (fork() > 0)
+	pipe(fd); 
+	p = fork();
+	if (p == 0)
 	{
-//		printf("Child process\n");
-		close(fd[0]);
-		dup2(fd[1], 1);
-		builtins(lst->command, lst->param, data);
-	}
-	else
-	{
-//		printf("Parent Process\n");
+		printf("Child process\n");
 		wait(NULL);
 		close(fd[1]);
 		mem = prs_mem(fd[0]);
-		close(fd[0]);
+
 	}
+	else
+	{
+		save_fd = dup(1);
+		printf("Parent Process\n");
+		close(fd[0]);
+		dup2(fd[1], 1);
+		builtins(lst->command, lst->param, data);
+		dup2(save_fd, 1);
+		close(fd[1]);
+		close(save_fd);
+//		close(fd[0]);
+	}
+//	close(fd[0]);
+//	close(fd[1]);
 //	printf("sucess[1]\n");
 	return(mem);
 //	return(NULL);
