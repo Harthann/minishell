c03aac2624c7f9160db33710e025de6281dbc252
Harthann <nca.eyraud@gmail.com>
2020-05-08 16:06:43 +0200

    fix: segfault for non command send

--- srcs/cmd_director.c
+++ srcs/cmd_director.c
@@ -6,9 +6,9 @@ int     cmd_director(t_cmd *list, t_data *data)
 
     while (list)
 	{
-		printf("Command : [%s]\n", list->command);
-		printf("Parameter : [%s]\n", list->param);
-        // builtins(list->command, list->param, data);
+		// printf("Command : [%s]\n", list->command);
+		// printf("Parameter : [%s]\n", list->param);
+        builtins(list->command, list->param, data);
 		list = list->next;
     }
     return (data->status);
--- srcs/ft_exec.c
+++ srcs/ft_exec.c
@@ -40,7 +40,6 @@ char	*get_path(char *exec, t_env_lst *env)
 		fd = open(cmd, O_RDONLY);
 	}
 	close(fd);
-	free(exec);
 	return (cmd);
 }
 
@@ -48,11 +47,17 @@ void	ft_exec(char *exec, t_data *data)
 {
 	char **argv;
 	char **env;
+	char *path;
 
+	path = NULL;
 	if (*exec != '/')
-		exec = get_path(exec, data->env_var);
-	argv = clean_params(exec, data->env_var, data);
+		path = get_path(exec, data->env_var);
+	if (path)
+		argv = clean_params(path, data->env_var, data);
+	else
+		argv = clean_params(exec, data->env_var, data);
 	env = ft_calloc(1, sizeof(char *));
 	execve(argv[0], &argv[0], env);
-	perror("execve");
+	perror(argv[0]);
+	free(exec);
 }
