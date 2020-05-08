f162c26097741863fe635f24a50eca2ced041ecf
bsteve456 <stevealan1@protonmail.com>
2020-05-08 14:01:58 +0200

    add display in fork loop

--- srcs/fork_dir/fork_parser.c
+++ srcs/fork_dir/fork_parser.c
@@ -1,5 +1,22 @@
 #include "minishell.h"
 
+int		ft_separate(char *command)
+{
+	if(command == NULL)
+		return (0);
+	else if (ft_memcmp(command, ">", 1) == 0)
+		return (1);
+	else if (ft_memcmp(command, ">>", 2) == 0)
+		return (1);
+	else if (ft_memcmp(command, "<", 1) == 0)
+		return (1);
+	else
+		return (0);
+}
+
+
+
+
 int		fork_parsing(t_cmd *list, t_data *data, int *count)
 {
 	char	*params_mem;
@@ -14,8 +31,13 @@ int		fork_parsing(t_cmd *list, t_data *data, int *count)
 		printf("success\n");
 		list = list->next;
 		*count += 1;
-		res = 0;
+		if (!list || ft_separate(list->command) == 0)
+		{
+			ft_putstr_fd(params_mem, 1);
+			res = 0;
+		}
+	//	res = 0;
 	}
-	printf("[%s]\n", params_mem);
+//	printf("[%s]\n", params_mem);
 	return (*count);
 }
