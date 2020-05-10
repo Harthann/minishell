9a140d531e0ff9f1c80da2b58c300e464c8b4711
bsteve456 <stevealan1@protonmail.com>
2020-05-10 17:34:44 +0200

    refactoring

--- srcs/fork_dir/reverse_fork.c
+++ srcs/fork_dir/reverse_fork.c
@@ -1,15 +1,46 @@
 #include "minishell.h"
 
+void	reverse_red_fork2(char *command, t_data *data, char **mem, int fd)
+{
+	int fds[2];
+	pid_t p;
+
+	pipe(fds);
+	p = fork();
+	if (p == 0)
+	{
+		close(fds[0]);
+		dup2(fds[1], 1);
+		dup2(fd, 0);
+		builtins(command, NULL, data);
+		_exit(0);
+	}
+	else
+	{
+		wait(NULL);
+		close(fds[1]);
+		free(*mem);
+		*mem = prs_mem(fds[0]);
+	}
+}
+
+void copy_file(char **mem, char *param)
+{
+	int f;
+
+	f = open(param, O_RDWR);
+	*mem = prs_mem(f);
+}
+
 void	reverse_red_fork(t_cmd *list, t_data *data, char **mem, int *count)
 {
 	char *command;
 	pid_t p;
-	int fd[2];
-	int f;
-	int fds[2];
-//	(void)data;
+	int *fd;
+
+	if(!(fd = malloc(sizeof(int) * 2)))
+		fd = 0;
 	pipe(fd);
-	
 	*count = 2;
 	command = list->command;
 	list = list->next;
@@ -17,39 +48,15 @@ void	reverse_red_fork(t_cmd *list, t_data *data, char **mem, int *count)
 	if(p == 0)
 	{
 		close(fd[0]);
-		f = open(list->param, O_RDWR);
-		*mem = prs_mem(f);
+		copy_file(mem, list->param);
 		dup2(fd[1], 1);
 		ft_putstr_fd(*mem, 1);
-		close(f);
 		_exit(0);
 	}
 	else
 	{
 		wait(NULL);
 		close(fd[1]);
-//		*mem = prs_mem(fd[0]);
-		pipe(fds);
-		p = fork();
-		if (p == 0)
-		{
-			close(fds[0]);
-			dup2(fds[1], 1);
-			close(fd[1]);
-			dup2(fd[0], 0);
-			builtins(command, NULL, data);
-			_exit(0);
-		}
-		else
-		{
-			wait(NULL);
-//			printf("eee\n");
-			close(fds[1]);
-			free(*mem);
-			*mem = prs_mem(fds[0]);
-	//		printf("[%s]\n", *mem);
-		}
-//	printf("[%s]\n", *mem);
+		reverse_red_fork2(command, data, mem, fd[0]);
 	}
-//	printf("Good\n");
 }
