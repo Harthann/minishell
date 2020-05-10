95f6efe8f83b2198f0d7420b090532931bdc13ab
bsteve456 <stevealan1@protonmail.com>
2020-05-10 17:46:45 +0200

    refactoring

--- srcs/fork_dir/pipe_fork.c
+++ srcs/fork_dir/pipe_fork.c
@@ -1,11 +1,38 @@
 #include "minishell.h"
 
+void	pipe_fork2(t_cmd *lst, t_data *data, char **mem, int fd)
+{
+	int *fds;
+	pid_t p;
+
+	if(!(fds = malloc(sizeof(int) * 2)))
+		fds = 0;
+	pipe(fds);
+	p = fork();
+	if(p == 0)
+	{
+		close(fds[0]);
+		dup2(fd, 0);
+		dup2(fds[1], 1);
+		builtins(lst->command, lst->param, data);
+		_exit(0);
+	}
+	else
+	{
+		wait(NULL);
+		close(fds[1]);
+		*mem = prs_mem(fds[0]);
+	}
+
+}
+
 void	pipe_fork(t_cmd *lst, t_data *data, char **mem, int *count)
 {
 	pid_t p;
-	int fd[2];
-	int fds[2];
+	int *fd;
 
+	if(!(fd = malloc(sizeof(int) * 2)))
+		fd = 0;
 	pipe(fd);
 	p = fork();
 	*count = 2;
@@ -20,22 +47,6 @@ void	pipe_fork(t_cmd *lst, t_data *data, char **mem, int *count)
 	{
 		wait(NULL);
 		close(fd[1]);
-		pipe(fds);
-		p = fork();
-		if(p == 0)
-		{
-			close(fds[0]);
-			dup2(fd[0], 0);
-			dup2(fds[1], 1);
-			builtins(lst->command, lst->param, data);
-			_exit(0);
-		}
-		else
-		{
-			wait(NULL);
-			close(fds[1]);
-			*mem = prs_mem(fds[0]);
-//			printf("[%s]\n", *mem);
-		}
+		pipe_fork2(lst, data, mem, fd[0]);
 	}
 }
