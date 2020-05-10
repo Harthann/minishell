f606f176e848be4086eb62655cc3b025fde6d753
bsteve456 <stevealan1@protonmail.com>
2020-05-10 17:14:20 +0200

    refactoring

--- srcs/fork_dir/normal_fork.c
+++ srcs/fork_dir/normal_fork.c
@@ -1,20 +1,24 @@
 #include "minishell.h"
 
+void	unset_export(t_cmd *list, t_data *data)
+{
+ 	if(ft_memcmp(list->command, "export", 6) == 0 && list->param != NULL)
+		builtins(list->command, list->param, data);
+	else if(ft_memcmp(list->command, "unset", 5) == 0)
+		builtins(list->command, list->param, data);
+
+}
+
+
 void	normal_fork(t_cmd *lst, t_data *data, char **mem, int *count)
 {
-	int fd[2];
-	int save_fd;
+	int		*fd;
+	int		save_fd;
+	pid_t	p;
 
 	*count = 1;
-//	char *mem;
-//	printf("hello\n");
-	pid_t p;
-//(void)data;
-//(void)lst;
-
-//if (!(fd = malloc(sizeof(int) * 2)))
-//		return (NULL);
-//	mem = NULL;
+	if(!(fd = malloc(sizeof(int) * 2)))
+		fd = 0;
 	pipe(fd);
 	p = fork();
 	if (p == 0)
@@ -24,7 +28,6 @@ void	normal_fork(t_cmd *lst, t_data *data, char **mem, int *count)
 		dup2(fd[1], 1);
 		builtins(lst->command, lst->param, data);
 		dup2(save_fd, 1);
-//		close(fd[1]);
 		close(save_fd);
 		_exit(0);
 	}
@@ -33,12 +36,6 @@ void	normal_fork(t_cmd *lst, t_data *data, char **mem, int *count)
 		wait(NULL);
 		close(fd[1]);
 		*mem = prs_mem(fd[0]);
-//		close(fd[0]);
+		unset_export(lst, data);
 	}
-//	close(fd[0]);
-//	close(fd[1]);
-//	printf("sucess[1]\n");
-//	return(mem);
-//	return(NULL);
-
 }
