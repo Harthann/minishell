73a82c03f65f3a6c01f905bcf629c2cff693e4bd
Harthann <nca.eyraud@gmail.com>
2020-10-27 09:03:35 +0100

    norming file

--- srcs/parsing/sort_redirection.c
+++ srcs/parsing/sort_redirection.c
@@ -6,7 +6,7 @@
 /*   By: nieyraud <nieyraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 11:11:32 by nieyraud          #+#    #+#             */
-/*   Updated: 2020/10/26 11:38:25 by nieyraud         ###   ########.fr       */
+/*   Updated: 2020/10/27 09:03:15 by nieyraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,13 +50,9 @@ void	order_redir(t_cmd **cmd)
 			tmp->next = NULL;
 			add_back(&left_redir, tmp);
 		}
-		if (tmp->command[0] != '<')
-			previous = tmp;
+		previous = tmp->command[0] != '<' ? tmp : previous;
 		tmp = next;
 	}
-	if (left_redir)
-	{
-		last_cmd(left_redir)->next = *cmd;
-		*cmd = left_redir;
-	}
+	add_back(&left_redir, *cmd);
+	*cmd = left_redir;
 }
